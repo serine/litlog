a1822561b58becaa6e8b1ab1f25245689cb9b8af
serine <tskiril@gmail.com>
2017-01-06 10:05:39 +1100

    renamed function

--- activate.zsh
+++ activate.zsh
@@ -16,12 +16,12 @@ fi
 
 PROMPT="(logit-2-$FILENAME)%{${fg[green]}%}[%m]:%{$reset_color%}%2~%{$reset_color%}%# " 
 
-_per-directory-history-addhistory() {
+_history_logger() {
   print -Sr -- "${1%%$'\n'}"
-  fc -ED -p $FILENAME
+  fc -p $FILENAME
 }
 HISTORY_IGNORE="fc*|logit*"
 # load hooks function
 # for builtin functions -U flag is recomeneded
 autoload -U add-zsh-hook
-add-zsh-hook zshaddhistory _per-directory-history-addhistory
+add-zsh-hook zshaddhistory _history_logger
--- deactivate.zsh
+++ deactivate.zsh
@@ -1,5 +1,5 @@
 
 PROMPT="%{${fg[green]}%}[%m]:%{$reset_color%}%2~%{$reset_color%}%# " 
-add-zsh-hook -d zshaddhistory _per-directory-history-addhistory
+add-zsh-hook -d zshaddhistory _history_logger
 unset HISTORY_IGNORE
 unset FILENAME
