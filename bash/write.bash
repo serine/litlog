edbf940381e85fa59f38a653fdd19dffd9b9fc69
serine <tskiril@gmail.com>
2017-01-10 23:02:29 +1100

    made a function for writing history and set if blocks to conditionally write out bits of information of interest

--- bash/write.bash
+++ bash/write.bash
@@ -2,18 +2,41 @@
 hist_all="$hist_file.all"
 hist_buffer="$hist_file.buffer"
 
-#cat $text_file >> $out_file
-echo "" >> $out_file
+write_hist() {
+  history > $hist_buffer
+  
+  if [[ ! -e $hist_all ]]
+  then
+    touch $hist_all
+  fi
+  
+  echo "\`\`\`" >> $out_file
+  comm -3 <(cut -f 7- $hist_buffer -d" " | sort ) <(cut -f 7- $hist_all -d" " | sort) | grep -v "logit*" >> $out_file
+  echo "\`\`\`" >> $out_file
+  
+  cat $hist_buffer > $hist_all
+}
 
-history > $hist_buffer
-
-if [[ ! -e $hist_all ]]
+if [[ -n $write_notes ]]
 then
-  touch $hist_all
+  echo "Writing out notes $write_notes"
+  cat $text_file >> $out_file
+  echo "" >> $out_file
 fi
 
-echo "\`\`\`" >> $out_file
-comm -3 <(cut -f 7- $hist_buffer -d" " | sort ) <(cut -f 7- $hist_all -d" " | sort) | grep -v "logit*" >> $out_file
-echo "\`\`\`" >> $out_file
+if [[ -n $write_history ]]
+then
+  echo "Writing out history $write_history"
+  write_hist
+fi
+if [[ -n $write_all ]]
+then
+  echo "Writing out all $write_all"
+  cat $text_file >> $out_file
+  echo "" >> $out_file
+  write_hist
+fi
 
-cat $hist_buffer > $hist_all
+unset write_all
+unset write_history
+unset write_notes
