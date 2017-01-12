8e0b04a7ffeb05831a866d09a5f06bc668016577
serine <tskiril@gmail.com>
2017-01-12 12:41:28 +1100

    remove write_all function as it was redundant and fixed a couple of typos

--- bash/write.bash
+++ bash/write.bash
@@ -1,25 +1,16 @@
 
-write_all() {
-  cat $litlog_notes_buffer >> $litlog_log_file
-  echo "" >> $litlog_log_file
-  cat $litlog_cmd_buffer >> $litlog_log_file
-  echo "Writing out notes then commands to $litlog_log_file"
-  rm $litlog_notes_buffer $littlog_cmd_buffer 
-  touch $littlog_notes $littlog_cmd_buffer
-}
-
 write_notes() {
   cat $litlog_notes_buffer >> $litlog_log_file
   echo "Writing out notes to $litlog_log_file"
   rm $litlog_notes_buffer
-  touch $littlog_notes_buffer
+  touch $litlog_notes_buffer
 }
 
 write_cmd() {
   echo "\`\`\`" >> $litlog_log_file
   cat $litlog_cmd_buffer >> $litlog_log_file
   echo "\`\`\`" >> $litlog_log_file
-  echo "Writing out notes to $litlog_log_file"
+  echo "Writing out commands to $litlog_log_file"
   rm $litlog_cmd_buffer
-  touch $littlog_cmd_buffer
+  touch $litlog_cmd_buffer
 }
